127d95ad7e23bb0d2f3ca0bcc56695a095f2455b
kiranvathank <kirankvathan@gmail.com>
2022-11-01 09:37:08 +0000

    Run Terraform Command with var-file

--- variables.tf
+++ variables.tf
@@ -30,4 +30,25 @@ variable "instance_2_name" {
   description = "Value of the Name Tag for the EC2 instance"
   type        = string
   default     = "instance-2"
+
+}
+
+variable "website_s3_bucket_1_name"{
+  description = "Value of the Name Tag for the S3 bucket"
+  type        = string
+}
+
+variable "website_s3_bucket_2_name"{
+  description = "Value of the Name Tag for the S3 bucket"
+  type        = string
+}
+
+variable "terraform"{
+  description = "Value of the Terraform Tag for the S3 bucket"
+  type        = string
+}
+
+variable "environment"{
+  description = "Value of the Environment Tag for the S3 bucket"
+  type        = string
 }
