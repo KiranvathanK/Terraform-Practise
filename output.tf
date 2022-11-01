91faa92334ae7187c295aaa663cc371268b0f984
kiranvathank <kirankvathan@gmail.com>
2022-11-01 07:54:21 +0000

    Use the self variable

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "instance_1_id" {
   description = "The ID of the instance-1"
   value       = try(aws_instance.e1.id)
 }
+
+output "instance_2_id" {
+  description = "The ID of the instance-2"
+  value       = try(aws_instance.e2.id)
+}
