ab4f45ce98fa5a3f090a4d0442d4eadbb5066cf5
kiranvathank <kirankvathan@gmail.com>
2022-11-03 10:20:31 +0000

    Locking Remote State

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
-backend "s3" {
-  bucket         = "git-terraform-best-practices-1"
-  key            = "terraform.tfstate"
-  region         = "us-east-1"
+   backend "s3" {
+   bucket         = "git-terraform-best-practices-1"
+   key            = "terraform.tfstate"
+   region         = "us-east-1"
+   dynamodb_table = "terraform_locks"
 }
 }
